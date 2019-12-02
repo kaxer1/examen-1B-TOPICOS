eeab94acdcb765874c29891f1184e41712833a05
kaxer1 <kevincuray41@gmail.com>
2019-12-02 03:26:00 -0500

    login Firestore

---
+++ SystemAppWeb/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBZlnop7pu31eewLOnRYiZRN39TgvVwFXo",
+    authDomain: "system-exam.firebaseapp.com",
+    databaseURL: "https://system-exam.firebaseio.com",
+    projectId: "system-exam",
+    storageBucket: "system-exam.appspot.com",
+    messagingSenderId: "986518207597",
+    appId: "1:986518207597:web:b99ee6f5f19605f50bd364",
+    measurementId: "G-MSDVWMFMET"
+  }
+};
